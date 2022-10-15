e8e684e1ce82cac7b1dffa65e37c6b1ee4abb10b
SalihTpc <salihtopcu@hotmail.com.tr>
2022-10-15 21:30:12 +0300

    User added.

--- configuration/Interfaces.ts
+++ configuration/Interfaces.ts
@@ -3,3 +3,14 @@ export interface Category {
   name: string;
   post_count: number;
 }
+
+export interface User {
+  key: string;
+  user: {
+    id: number;
+    username: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+  };
+}
